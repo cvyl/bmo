04e333b1de69c2a728af87ff117adad7f223950e
跨性别 <cvyl@pm.me>
2024-06-23 19:00:41 +0200

    fix: crazy

--- src/routes.ts
+++ src/routes.ts
@@ -389,6 +389,12 @@ const getFile = async (request: IRequestStrict, env: Env, ctx: ExecutionContext)
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content="boymoder.org" />
             <meta property="og:image" content="${imageUrl}" />
+			<meta property="og:image:width" content="400" />
+			<meta property="og:image:height" content="400" />
+			<meta property="og:description" content="boymoder.org" />
+			<meta property="og:url" content="https://boymoder.org/${id}" />
+			<meta property="og:type" content="website" />
+			
             <title>boymoder.org</title>
         </head>
         <body>
