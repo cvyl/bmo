f8fa96f2de6897dfb92f5045eb5dfc3eee6b7469
跨性别 <cvyl@pm.me>
2024-06-23 19:03:26 +0200

    fix: image
    if this works im rebuilding from source with new forgotten knowledge of the ancient times

--- src/routes.ts
+++ src/routes.ts
@@ -389,8 +389,8 @@ const getFile = async (request: IRequestStrict, env: Env, ctx: ExecutionContext)
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content="boymoder.org" />
             <meta property="og:image" content="${imageUrl}" />
-			<meta property="og:image:width" content="400" />
-			<meta property="og:image:height" content="400" />
+			<meta name="twitter:card" content="summary_large_image">
+			
 			<meta property="og:description" content="boymoder.org" />
 			<meta property="og:url" content="https://boymoder.org/${id}" />
 			<meta property="og:type" content="website" />
